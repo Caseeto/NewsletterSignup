664f8c2640cde1b863ae89e24aa7faf2ac6fb4cc
TrexASHUoP <92679039+Caseeto@users.noreply.github.com>
2023-07-30 11:43:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const https = require('https');
 const app = express();
-//require('dotenv').config();
+
 const API_key = process.env.API_KEY;
 
 app.use(express.static("public"));
